fefacc73fd22a9b323b753384274bea2f4372028
Drew Watson <tua07182@temple.edu>
2019-05-17 03:10:06 -0400

    upload

---
+++ LeafletTutorial/myJavaScript.js
@@ -0,0 +1,13 @@
+﻿/*JQuery for menu burger*/
+$(document).ready(function () {
+    $('#nav-btn').click(function () {
+        $(this).next().toggle();
+    });
+});
+
+$(window).resize(function () {
+    if ($(window).width() > 600)
+        $('.navList').css("display", "block");
+    else
+        $('.navList').css("display", "none");
+});
